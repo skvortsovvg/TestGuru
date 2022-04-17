067dc4d21db8c3029a6b1738922d9c46a9ebbf43
Skvortsovvg <vovanches@i.ua>
2022-04-17 10:21:48 +0300

    correct remarks 2

--- db/migrate/20220415152016_create_tests.rb
+++ db/migrate/20220415152016_create_tests.rb
@@ -1,8 +1,8 @@
 class CreateTests < ActiveRecord::Migration[7.0]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
+      t.string :title, null: false
+      t.integer :level, null: false
       t.references :category, null: false, foreign_key: true
       t.references :author, null: false, foreign_key: { to_table: 'users' }
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_15_153957) do
   create_table "answers", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -21,13 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_153957) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -35,8 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_153957) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level", default: 1
+    t.string "title", null: false
+    t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
     t.integer "author_id", null: false
     t.datetime "created_at", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_153957) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
