28d76710e0cc82a4ad1bd5852f0bbd18008747f9
Skvortsovvg <vovanches@i.ua>
2022-04-17 18:51:12 +0300

    correct remarks 3

--- db/migrate/20220415153957_add_answers_correct_column.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswersCorrectColumn < ActiveRecord::Migration[7.0]
-  def change
-    add_column(:answers, :correct, :boolean, default: false, null: false)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema[7.0].define(version: 2022_04_15_153957) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "correct", default: false, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
