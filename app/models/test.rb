e363a6e6b8a23a6e2ae1960e6de5aac624f3e69f
Skvortsovvg <vovanches@i.ua>
2022-04-20 16:32:09 +0300

    results & methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.find_by_title(str)
-    self.pluck('title').where("title LIKE %:param%", param: str).order('title DESC')
+    self.select(:title).where("title LIKE :param", param: "%#{str}%" ).order('title DESC').pluck('title')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def my_tests(level)
+    # select DISTINCT tests.title from results inner join tests on tests.id = results.test_id and results.user_id = 1;
+    Result.joins(:tests).where(user_id: self.id, level: level)
+  end
 end
