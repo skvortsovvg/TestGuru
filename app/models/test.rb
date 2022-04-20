2b190a0237808ffa48fefc5a8b62ee6b87d09c73
Skvortsovvg <vovanches@i.ua>
2022-04-20 21:00:04 +0300

    correct remarks 3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   def self.find_by_title(str)
-    self.where("title LIKE :param", param: "%#{str}%").order('title DESC').pluck(:title)
+    # select * from tests inner join categories on tests.category_id = categories.id and categories.title like "%end%"
+    self.joins('join categories on tests.category_id = categories.id').where('categories.title like :str', str: "%#{str}%").order('title DESC')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def my_tests(level = nil)
     # select DISTINCT tests.title from results inner join tests on tests.id = results.test_id and results.user_id = 1 and tests.level = 1;
-    Test.joins("inner join results on tests.id = results.test_id").where("results.user_id = :user_id and (tests.level = :level or :level is null)", user_id: self.id, level: level).distinct.pluck
+    Test.joins("inner join results on tests.id = results.test_id").where("results.user_id = :user_id and (tests.level = :level or :level is null)", user_id: self.id, level: level).distinct
   end
 end
