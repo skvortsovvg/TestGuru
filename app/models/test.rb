14153fa0bab769f21056d1c7d1c9da6d0d131dcc
Skvortsovvg <vovanches@i.ua>
2022-04-22 12:18:14 +0300

    associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,10 @@
 class Test < ApplicationRecord
+  has_many :results
+  has_many :users, through: :results
   has_many :questions
   belongs_to :category
-  
+  belongs_to :author, class_name: "User"
+
   def self.find_by_title(str)
     # select * from tests inner join categories on tests.category_id = categories.id and categories.title like "%end%"
     self.joins('join categories on tests.category_id = categories.id').where('categories.title like :str', str: "%#{str}%").order('title DESC')
