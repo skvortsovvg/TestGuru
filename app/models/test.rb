9041f531c662c93e51b10f0dbaabb95cf656a4e8
Skvortsovvg <vovanches@i.ua>
2022-04-23 16:04:51 +0300

    correct remarks

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,6 @@ class Test < ApplicationRecord
 
   def self.find_by_title(str)
     # select * from tests inner join categories on tests.category_id = categories.id and categories.title like "%end%"
-    self.joins('join categories on tests.category_id = categories.id').where('categories.title like :str', str: "%#{str}%").order('title DESC')
+    self.joins(:category).where('categories.title like :str', str: "%#{str}%").order('title DESC')
   end
 end
