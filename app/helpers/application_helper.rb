08c8c49b2b6d0e5db4b23f51a13194463260bc15
Skvortsovvg <vovanches@i.ua>
2022-05-16 17:13:19 +0300

    solution for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def this_year
+    Time.now.year
+  end
+  def github_url(author, repo)
+    # "https://github.com/"skvortsovvg/TestGuru
+    "https://github.com/#{author.strip}/#{repo.strip}"
+  end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,5 @@
 module TestsHelper
+  def question_header(question, test)
+    question.new_record? ? "Create New #{test.title} Question" : "Edit #{test.title} Question"
+  end
 end
