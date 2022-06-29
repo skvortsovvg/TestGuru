2eb36e9a11031a2c29d9ee3b6dd68a2a7c5a553d
Skvortsovvg <vovanches@i.ua>
2022-06-29 18:22:33 +0300

    gists list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'gists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   scope "(:locale)", locale: /en|ru|/ do
@@ -16,7 +17,7 @@ Rails.application.routes.draw do
     end
 
     namespace :admin do
-      resources :gists
+      resources :gists, only: :index
       resources :tests do
         resources :questions, shallow: true do
           resources :answers, shallow: true
