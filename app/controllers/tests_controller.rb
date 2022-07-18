e7f9e62f20db76931eefadb252eeeec8fdc1ce88
Skvortsovvg <vovanches@i.ua>
2022-07-18 13:45:24 +0300

    action mailer configure

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  # skip_before_action :authenticate_user!, only: :index
   before_action :set_test, only: %i[start]
 
   def index
