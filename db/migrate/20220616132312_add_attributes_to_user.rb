afdc8a5a3697f639c5b8b001799297ad72cffd2f
Skvortsovvg <vovanches@i.ua>
2022-07-26 10:59:19 +0300

    correstions

--- db/migrate/20220616132312_add_attributes_to_user.rb
+++ db/migrate/20220616132312_add_attributes_to_user.rb
@@ -2,5 +2,6 @@ class AddAttributesToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :type, :string, null: false, default: 'User'
     add_column :users, :last_name, :string
+    rename_column :users, :name, :first_name
   end
 end
